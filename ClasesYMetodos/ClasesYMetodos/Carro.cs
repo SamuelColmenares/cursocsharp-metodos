1a6811ced3e4bcc286f67a1c8c250cd55674bff5
Samuel Colmenares <locoucla@hotmail.com>
2021-01-22 19:34:12 -0500

    Clase 3

---
+++ ClasesYMetodos/ClasesYMetodos/Carro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClasesYMetodos
+{
+    abstract class Carro
+    {
+        public void Arrancar()
+        {
+            Console.WriteLine("Arranca carro.");
+        }
+
+        protected void AdicionarGasolina(int galones)
+        {
+            Console.WriteLine("llene {0} galones de gasolina del carro.", galones);
+        }
+
+        public virtual int Manejar(int km, int velocidad)
+        {
+            return 1;
+        }
+
+        public abstract double ObtenerMaxVelocidad();
+    }
+}
---
+++ ClasesYMetodos/ClasesYMetodos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClasesYMetodos
+{
+    class Program : Carro
+    {
+        static void Main(string[] args)
+        {
+            Program carro = new Program();
+            carro.Arrancar();
+            carro.AdicionarGasolina(5);
+            int manejar = carro.Manejar(5, 10);
+            var aa = 2;
+            Console.WriteLine("Valor {1} de funcioni manejar {0}", manejar, aa);
+            double velocidadMax = carro.ObtenerMaxVelocidad();
+            Console.WriteLine("Mi velocidad max es {0} ", velocidadMax);
+            string distancia = carro.Distancia(10, 30, "Millas.");
+            int dist2 = carro.Distancia(5, 20);
+            Console.WriteLine("- {0} - {1}", distancia, dist2);
+        }
+
+        public override double ObtenerMaxVelocidad()
+        {
+            return 201.4;
+        }
+
+        public string Distancia(int inicio, int fin, string medida = "Km")
+        {
+            return $"Mi distancia fue: {fin - inicio} {medida}";
+        }
+
+        public int Distancia(int inicio, int fin)
+        {
+            return fin - inicio;
+        }
+
+    }
+
+
+}
