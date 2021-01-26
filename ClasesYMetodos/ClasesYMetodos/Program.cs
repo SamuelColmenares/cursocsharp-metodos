0aebfd5690e1e7e455a552bfc4ac14adac94b9c1
Samuel Colmenares <locoucla@hotmail.com>
2021-01-25 19:39:01 -0500

    Clase 3

--- ClasesYMetodos/ClasesYMetodos/Program.cs
+++ ClasesYMetodos/ClasesYMetodos/Program.cs
@@ -4,6 +4,10 @@ namespace ClasesYMetodos
 {
     class Program : Carro
     {
+        delegate double Suma(float numA, float numB1); //<float, float, double>
+
+        
+
         static void Main(string[] args)
         {
             Program carro = new Program();
@@ -17,6 +21,53 @@ namespace ClasesYMetodos
             string distancia = carro.Distancia(10, 30, "Millas.");
             int dist2 = carro.Distancia(5, 20);
             Console.WriteLine("- {0} - {1}", distancia, dist2);
+            Console.WriteLine("- ============================ ");
+            // Delegados.....
+            //Suma add = delegate (float numA, float numB) { return numA + numB; };
+            Suma add = (float numA, float numB) => numA + numB;
+
+            Func<int, int, double> Resta = (int a, int b) => b - a;
+
+            double res = add(11.11f, 4.12f);
+            Console.WriteLine("- {0} ", res);
+
+            Action<string,int,bool> operacion = carro.Operacion;
+            Func<int, int, double> Suma = (int valA, int valB) =>
+            {
+                Console.WriteLine("valr a: {0}", valA);
+                if (valA >= 144)
+                {
+                    return valA;
+                }
+                else
+                {
+                    return 0;
+                }
+            };
+
+            Suma(0, 1);
+
+
+
+        }
+
+        public void Calcular (Action<int> action, int valA, bool a)
+        {
+            if (a)
+            {
+                action(valA);
+            }
+        }
+
+        public void Operacion(string dato, int dato2, bool val)
+        {
+            Console.WriteLine(" dato - {0} - {1} - {2}", dato, dato2, val);
+        }
+
+        public double ImplSuma(float valA, float valorB)//<float, float, double>
+        {
+            
+            return valA + valorB;
         }
 
         public override double ObtenerMaxVelocidad()
---
+++ ClasesYMetodos/MetodosAsincronos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MetodosAsincronos
+{
+    public class Program
+    {
+        static Task Main(string[] args) => EjecutarDemoraAsync();        
+        static async Task EjecutarDemoraAsync()
+        {
+            DateTime inicio = DateTime.Now;
+            /*Task<int> demora = Demora();
+            Console.WriteLine("Ejecucion tarea async.. {0} ", (DateTime.Now - inicio));
+
+            Task<int> demora2 = Demora2();
+            Console.WriteLine("Ejecucion tarea async 2.. {0}", (DateTime.Now - inicio));
+
+            int resultado = await demora;
+            Console.WriteLine("Respuesta tarea async.. {0} Tiempo: {1}", resultado, (DateTime.Now - inicio));
+            int resultado2 = await demora2;
+            Console.WriteLine("Respuesta tarea async 2.. {0} Tiempo: {1} ", resultado2, (DateTime.Now - inicio));*/
+            int demora = await Demora();
+            Console.WriteLine("Ejecucion tarea async.. {0} ", (DateTime.Now - inicio));
+
+            int demora2 = await Demora2();
+            Console.WriteLine("Ejecucion tarea async 2.. {0}", (DateTime.Now - inicio));            
+        }
+
+        public static async Task<int> Demora()
+        {
+            Console.WriteLine("Inicia metodo");
+            await Task.Delay(4000);
+            Console.WriteLine("fin delay");
+            return 5;
+        }
+
+        public static async Task<int> Demora2()
+        {
+            Console.WriteLine("Inicia metodo2");
+            await Task.Delay(7000);
+            Console.WriteLine("fin delay 2");
+            return 15;
+        }
+
+    }
+}
